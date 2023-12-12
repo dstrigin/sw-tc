5837fbb37dd5391244a7c088ba87a2f9db290c98
Dima <dmitriystrigin04@gmail.com>
2023-12-12 15:38:23 +0300

    создал простую программу на dart

---
+++ bonus/bin/bonus.dart
@@ -0,0 +1,5 @@
+import 'package:bonus/bonus.dart' as bonus;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bonus.calculate()}!');
+}
---
+++ bonus/test/bonus_test.dart
@@ -0,0 +1,8 @@
+import 'package:bonus/bonus.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
