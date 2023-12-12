b5e02c2710ad18acc5f825caa0eee03677f4d06a
Dima <dmitriystrigin04@gmail.com>
2023-12-12 17:25:54 +0300

    Update bonus_test.dart

--- bonus/test/bonus_test.dart
+++ bonus/test/bonus_test.dart
@@ -3,6 +3,18 @@ import 'package:test/test.dart';
 
 void main() {
   test('calculate1', () {
-    expect(calculate(6, 7), 41);
+    expect(calculate(6, 7), 42);
+  });
+  test('calculate2', () {
+    expect(calculate(11, 0), 0);
+  });
+  test('calculate3', () {
+    expect(calculate(2, -3), -6);
+  });
+  test('calculate4', () {
+    expect(calculate(10, 10), 100);
+  });
+  test('calculate5', () {
+    expect(calculate(1, -37), -37);
   });
 }
